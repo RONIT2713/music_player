60367d72afcdf2ff8f96c0b89d71cff43e85f2f9
RON <ronitrai270@gmail.com>
2026-01-16 03:09:35 +0530

    UNDER DEVELOPMENT

--- avatar.js
+++ avatar.js
@@ -2,51 +2,52 @@ const INITIAL_AVATAR_DATA = [
   {
     id: 1,
     name: "a1",
-    file: "a1.jpg"
+    file: "a1.webp"
   },
   {
     id: 2,
     name: "a2",
-    file: "a2.jpg"
+    file: "a2.webp"
   },
   {
     id: 3,
     name: "a3",
-    file: "a3.jpg"
+    file: "a3.webp"
   },
   {
     id: 4,
     name: "a4",
-    file: "a4.jpg"
+    file: "a4.webp"
   },
+
   {
     id: 5,
     name: "a5",
-    file: "a5.jpg"
+    file: "a5.webp"
   },
   {
     id: 6,
     name: "a6",
-    file: "a6.jpg"
+    file: "a6.webp"
   },
   {
     id: 7,
     name: "a7",
-    file: "a7.jpg"
+    file: "a7.webp"
   },
   {
     id: 8,
     name: "a8",
-    file: "a8.jpg"
+    file: "a8.webp"
   },
   {
     id: 9,
     name: "a9",
-    file: "a9.jpg"
+    file: "a9.webp"
   },
   {
     id: 10,
     name: "a10",
-    file: "a10.jpg"
+    file: "a10.webp"
   }
 ];
--- banner.js
+++ banner.js
@@ -2,31 +2,31 @@ const INITIAL_BANNER_DATA = [
   {
     id: 1,
     name: "b1",
-    file: "b1.jpg"
+    file: "b1.webp"
   },
   {
     id: 2,
     name: "b2",
-    file: "b2.jpg"
+    file: "b2.webp"
   },
   {
     id: 3,
     name: "b3",
-    file: "b3.jpg"
+    file: "b3.webp"
   },
   {
     id: 4,
     name: "b4",
-    file: "b4.jpg"
+    file: "b4.webp"
   },
   {
     id: 5,
     name: "b5",
-    file: "b5.jpg"
+    file: "b5.webp"
   },
   {
     id: 6,
     name: "b6",
-    file: "b6.jpg"
+    file: "b6.webp"
   }
 ];
